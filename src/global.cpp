02b3b8eda63f436fe48086ab6377904adb698578
Easychips <saberxnekopokemoncheezer@gmail.com>
2025-09-21 13:51:21 -0500

    Update global.cpp

--- src/global.cpp
+++ src/global.cpp
@@ -19,147 +19,98 @@ std::vector<std::string> whitelist;
 
 std::unordered_map<RE::TESModel*, std::string> ModelsAndOriginalFilePaths;
 
+// nodeName (lowercased) -> template mesh path
+std::unordered_map<std::string, std::string> baseMeshesAndNiNodeToAttach = {
+    {"candlehornfloor01", "Candle_Animated.nif"},
+    {"candlehorntable01", "Candle_Animated.nif"},
+    {"candlehornwall01", "Candle_Animated.nif"},
+    {"candlelanternwithcandle01", "Candle_Animated.nif"},
+    {"impcandelabracandle01", "Candle_Animated.nif"},
+    {"impcandelabracandle02", "Candle_Animated.nif"},
+    {"impcandle01", "Candle_Animated.nif"},
+    {"impwallsconce02candleon01", "Candle_Animated.nif"},
+    {"impwallsconcecandle01", "Candle_Animated.nif"},
+    {"impwallsconcecandle02", "Candle_Animated.nif"},
+    {"glazedcandlesstatic02", "Candle_Animated.nif"},
+    {"glazedcandles01", "Candle_Animated.nif"},
+    {"mpscandleflame01.nif", "Candle_Animated.nif"},
 
-std::map<std::array<std::string, 13>, RE::NiPointer<RE::NiNode>> baseMeshesAndNiNodeToAttach{
-    
-    // Candle_Animated.nif
-{{
-   "meshes/clutter/candles/candlehornfloor01.nif",
-   "meshes/clutter/candles/candlehorntable01.nif",
-   "meshes/clutter/candles/candlehornwall01.nif",
-   "meshes/clutter/common/candlelanternwithcandle01.nif",
-   "meshes/clutter/imperial/impcandelabracandle01.nif",
-   "meshes/clutter/imperial/impcandelabracandle02.nif",
-   "meshes/clutter/imperial/impcandle01.nif",
-   "meshes/clutter/imperial/impwallsconce02candleon01.nif",
-   "meshes/clutter/imperial/impwallsconcecandle01.nif",
-   "meshes/clutter/imperial/impwallsconcecandle02.nif",
-   "meshes/clutter/candles/glazedcandlesstatic02.nif",
-   "meshes/clutter/candles/glazedcandles01.nif",
-   "meshes/clutter/candles/mpscandleflame01.nif"
-}, nullptr},
+    {"candlehornchandelier01", "Chandelier_Animated.nif"},
+    {"candlehornchandelier02", "Chandelier_Animated.nif"},
+    {"impchandelliercandle01", "Chandelier_Animated.nif"},
+    {"impchandelliercandle02", "Chandelier_Animated.nif"},
 
+    {"mgmagicfirepillarsmall", "College Magic Fire Small_Animated.nif"},
+    {"mgmagicfirepillarmidden01", "College Magic Fire_Animated.nif"},
+    {"mgmagicfirepillar01", "College Magic Pillar_Animated.nif"},
 
-    // Chandelier_Animated.nif
-    {{"meshes/clutter/candles/candlehornchandelier01.nif",
-      "meshes/clutter/candles/candlehornchandelier02.nif",
-      "meshes/clutter/imperial/impchandelliercandle01.nif",
-      "meshes/clutter/imperial/impchandelliercandle02.nif"}, nullptr},
+    {"darkelflantern", "Dark Elf Lantern_Animated.nif"},
 
-    // College Magic Fire Small_Animated.nif
-    {{"meshes/effects/mgmagicfirepillarsmall.nif"}, nullptr},
+    {"dwechandelier01bareonsputter", "Dwemer Chandelier Sputter_Animated.nif"},
+    {"dwechandelier01onsputter", "Dwemer Chandelier Sputter_Animated.nif"},
+    {"dwechandelier01simpleonsputter", "Dwemer Chandelier Sputter_Animated.nif"},
+    {"dwechandelier02onsputter", "Dwemer Chandelier Sputter_Animated.nif"},
 
-    // College Magic Fire_Animated.nif
-    {{"meshes/effects/mgmagicfirepillarmidden01.nif"}, nullptr},
+    {"dwechandelier01bareon", "Dwemer Chandelier_Animated.nif"},
+    {"dwechandelier01on", "Dwemer Chandelier_Animated.nif"},
+    {"dwechandelier01simpleon", "Dwemer Chandelier_Animated.nif"},
+    {"dwechandelier02on", "Dwemer Chandelier_Animated.nif"},
 
-    // College Magic Pillar_Animated.nif
-    {{"meshes/effects/mgmagicfirepillar01.nif"}, nullptr},
+    {"dwefloorlanternonsputter", "Dwemer Light Sputter_Animated.nif"},
+    {"dwewallsconce01onsputter", "Dwemer Light Sputter_Animated.nif"},
+    {"dwewallsconce02onsputter", "Dwemer Light Sputter_Animated.nif"},
+    {"dwewallsconcesmall01onsputter", "Dwemer Light Sputter_Animated.nif"},
+    {"dwewallsconcesmall02onsputter", "Dwemer Light Sputter_Animated.nif"},
+    {"fxdwegreenflamesputter", "Dwemer Light Sputter_Animated.nif"},
 
-    // Dark Elf Lantern_Animated.nif
-    {{"meshes/dlc02/clutter/darkelflantern.nif"}, nullptr},  
+    {"dwefloorlanternon", "Dwemer Light_Animated.nif"},
+    {"dwewallsconce01on", "Dwemer Light_Animated.nif"},
+    {"dwewallsconce02on", "Dwemer Light_Animated.nif"},
+    {"dwewallsconcesmall01on", "Dwemer Light_Animated.nif"},
+    {"dwewallsconcesmall02on", "Dwemer Light_Animated.nif"},
+    {"fxdwegreenflame", "Dwemer Light_Animated.nif"},
+    {"fxdwegreenflamecalm", "Dwemer Light_Animated.nif"},
 
-    // Dwemer Chandelier Sputter_Animated.nif
-    {{"meshes/dungeons/dwemer/dwechandelier01bareonsputter.nif",
-      "meshes/dungeons/dwemer/dwechandelier01onsputter.nif",
-      "meshes/dungeons/dwemer/dwechandelier01simpleonsputter.nif",
-      "meshes/dungeons/dwemer/dwechandelier02onsputter.nif"}, nullptr},
+    {"c06fxfirebluewithembersheavy", "Fires Blue_Animated.nif"},
+    {"fxfirewithembers01_cheap", "Fires Small_Animated.nif"},
+    {"fxfirewithembers03", "Fires Small_Animated.nif"},
+    {"fxfiresovngarde", "Fires Sovngarde_Animated.nif"},
 
-    // Dwemer Chandelier_Animated.nif
-    {{"meshes/dungeons/dwemer/dwechandelier01bareon.nif",
-      "meshes/dungeons/dwemer/dwechandelier01on.nif",
-      "meshes/dungeons/dwemer/dwechandelier01simpleon.nif",
-      "meshes/dungeons/dwemer/dwechandelier02on.nif"}, nullptr},
+    {"fxfirewithembers01", "Fires_Animated.nif"},
+    {"fxfirewithembers01_lite", "Fires_Animated.nif"},
+    {"fxfirewithembers02", "Fires_Animated.nif"},
+    {"fxfirewithembers02toggle", "Fires_Animated.nif"},
+    {"fxfirewithembersheavytoggle", "Fires_Animated.nif"},
+    {"fxfirewithemberslogs01", "Fires_Animated.nif"},
+    {"campfire01burning", "Fires_Animated.nif"},
+    {"giantcampfire01burning", "Fires_Animated.nif"},
+    {"fireplacewood01burning", "Fires_Animated.nif"},
+    {"campfire01landburning", "Fires_Animated.nif"},
 
-    // Dwemer Light Sputter_Animated.nif
-    {{"meshes/dungeons/dwemer/dwefloorlanternonsputter.nif",
-      "meshes/dungeons/dwemer/dwewallsconce01onsputter.nif",
-      "meshes/dungeons/dwemer/dwewallsconce02onsputter.nif",
-      "meshes/dungeons/dwemer/dwewallsconcesmall01onsputter.nif",
-      "meshes/dungeons/dwemer/dwewallsconcesmall02onsputter.nif",
-      "meshes/effects/fxdwegreenflamesputter.nif"}, nullptr},
+    {"giantcampfire01burningnoland", "Giant Fires_Animated.nif"},
 
-    // Dwemer Light_Animated.nif
-    {{"meshes/dungeons/dwemer/dwefloorlanternon.nif",
-      "meshes/dungeons/dwemer/dwewallsconce01on.nif",
-      "meshes/dungeons/dwemer/dwewallsconce02on.nif",
-      "meshes/dungeons/dwemer/dwewallsconcesmall01on.nif",
-      "meshes/dungeons/dwemer/dwewallsconcesmall02on.nif",
-      "meshes/effects/fxdwegreenflame.nif",
-      "meshes/effects/fxdwegreenflamecalm.nif"}, nullptr},
+    {"dlc2castlekarstaagtorch", "Karstaag Ice Fires_Animated.nif"},
+    {"tg09nocturnalfxfirepurple", "Nocturnal Purple Fire_Animated.nif"},
+    {"cascoffinpuzzlebrazier01", "Puzzle Purple Fire_Animated.nif"},
 
-         // Fires Blue_Animated.nif
-    {{"meshes/effects/c06fxfirebluewithembersheavy.nif"}, nullptr},
+    {"removabletorch01", "Torch Sconce_Animated.nif"},
+    {"torchpermanent01", "Torch Sconce_Animated.nif"},
+    {"torchsconce01", "Torch Sconce_Animated.nif"},
 
-    // Fires Small_Animated.nif
-    {{"meshes/effects/fxfirewithembers01_cheap.nif",
-      "meshes/effects/fxfirewithembers03.nif"}, nullptr},
+    {"sc_crater01", "Soul Cairn_Animated.nif"},
+    {"sc_lightwell01", "Soul Cairn_Animated.nif"},
+    {"aposcryetrigger01", "Apoc Trigger Lamp_Animated.nif"},
 
-    // Fires Sovngarde_Animated.nif
-    {{"meshes/effects/fxfiresovngarde.nif"}, nullptr},
-
-    // Fires_Animated.nif
-    {{"meshes/effects/fxfirewithembers01.nif",
-      "meshes/effects/fxfirewithembers01_lite.nif",
-      "meshes/effects/fxfirewithembers02.nif",
-      "meshes/effects/fxfirewithembers02toggle.nif",
-      "meshes/effects/fxfirewithembersheavytoggle.nif",
-      "meshes/effects/fxfirewithemberslogs01.nif",
-      "meshes/clutter/woodfires/campfire01burning.nif",
-      "meshes/clutter/giant/giantcampfire01burning.nif",
-      "meshes/clutter/woodfires/fireplacewood01burning.nif",
-      "meshes/clutter/woodfires/campfire01landburning.nif"}, nullptr},
-
-   // Giant Fires_Animated.nif
-    {{
-    "meshes/clutter/giant/giantcampfire01burning.nif",
-    "meshes/clutter/giant/giantcampfire01burningnoland.nif"
-    }, nullptr},
-
-// Karstaag Ice Fires_Animated.nif
-    {{ "meshes/dlc02/effects/dlc2castlekarstaagtorch.nif" }, nullptr},
-
-// Nocturnal Purple Fire_Animated.nif
-    {{ "meshes/effects/tg09nocturnalfxfirepurple.nif" }, nullptr},
-
-// Puzzle Purple Fire_Animated.nif
-    {{ "meshes/dlc01/dungeons/castle/animated/cascoffinpuzzlebrazier/cascoffinpuzzlebrazier01.nif" }, nullptr},
-
-// Torch Sconce_Animated.nif
-    {{
-    "meshes/clutter/common/removabletorch01.nif",
-    "meshes/clutter/common/torchpermanent01.nif",
-    "meshes/clutter/common/torchsconce01.nif"
-    }, nullptr},
-
-// Soul Cairn_Animated.nif
-    {{
-    "meshes/dlc01/soulcairn/sc_crater01.nif",
-    "meshes/dlc01/soulcairn/sc_lightwell01.nif"
-    }, nullptr},
-
-// Apoc Trigger Lamp_Animated.nif
-    {{ "meshes/dlc02/dungeons/apocrypha/animated/aposcryetrigger01/aposcryetrigger01.nif" }, nullptr},
-
-// Apoc Lamp_Animated.nif
-    {{
-    "meshes/dlc02/dungeons/apocrypha/clutter/apolightpost01.nif",
-    "meshes/dlc02/dungeons/apocrypha/clutter/apolightpost02.nif"
-    }, nullptr},
-
-// Apoc Head Sconce Green_Animated.nif
-    {{
-    "meshes/dlc02/dungeons/apocrypha/clutter/apoheadscone02.nif",
-    "meshes/dlc02/dungeons/apocrypha/clutter/apomouthsconce02.nif"
-    }, nullptr},
-
-// Apoc Head Sconce Blue_Animated.nif
-    {{
-    "meshes/dlc02/dungeons/apocrypha/clutter/apoheadscone03.nif",
-    "meshes/dlc02/dungeons/apocrypha/clutter/apomouthsconce03.nif"
-    }, nullptr},
-    
+    {"apolightpost01", "Apoc Lamp_Animated.nif"},
+    {"apolightpost02", "Apoc Lamp_Animated.nif"},
+    {"apoheadscone02", "Apoc Head Sconce Green.nif"},
+    {"apomouthsconce02", "Apoc Head Sconce Green.nif"},
+    {"apoheadscone03", "Apoc Head Sconce Blue.nif"},
+    {"apomouthsconce03", "Apoc Head Sconce Blue.nif"}
 };
 
+
+/*
 std::array<std::string, 26> templateNames = {
    "Candle_Animated.nif",
     "Chandelier_Animated.nif",
@@ -189,97 +140,80 @@ std::array<std::string, 26> templateNames = {
     "Hand Held Torch_Animated.nif"
 }; 
 
- std::unordered_set<std::string> allMeshes{
-    "meshes/clutter/candles/candlehornfloor01.nif",
-    "meshes/clutter/candles/candlehorntable01.nif",
-    "meshes/clutter/candles/candlehornwall01.nif",
-    "meshes/clutter/common/candlelanternwithcandle01.nif",
-    "meshes/clutter/imperial/impcandelabracandle01.nif",
-    "meshes/clutter/imperial/impcandelabracandle02.nif",
-    "meshes/clutter/imperial/impcandle01.nif",
-    "meshes/clutter/imperial/impwallsconce02candleon01.nif",
-    "meshes/clutter/imperial/impwallsconcecandle01.nif",
-    "meshes/clutter/imperial/impwallsconcecandle02.nif",
-
-    "meshes/clutter/candles/candlehornchandelier01.nif",
-    "meshes/clutter/candles/candlehornchandelier02.nif",
-    "meshes/clutter/imperial/impchandelliercandle01.nif",
-    "meshes/clutter/imperial/impchandelliercandle02.nif",
-
-    "meshes/effects/mgmagicfirepillarsmall.nif",
-    "meshes/effects/mgmagicfirepillarmidden01.nif",
-    "meshes/effects/mgmagicfirepillar01.nif",
-    "meshes/dlc02/clutter/darkelflantern.nif",
-
-    "meshes/dungeons/dwemer/dwechandelier01bareonsputter.nif",
-    "meshes/dungeons/dwemer/dwechandelier01onsputter.nif",
-    "meshes/dungeons/dwemer/dwechandelier01simpleonsputter.nif",
-    "meshes/dungeons/dwemer/dwechandelier02onsputter.nif",
-
-    "meshes/dungeons/dwemer/dwechandelier01bareon.nif",
-    "meshes/dungeons/dwemer/dwechandelier01on.nif",
-    "meshes/dungeons/dwemer/dwechandelier01simpleon.nif",
-    "meshes/dungeons/dwemer/dwechandelier02on.nif",
-
-    "meshes/dungeons/dwemer/dwefloorlanternonsputter.nif",
-    "meshes/dungeons/dwemer/dwewallsconce01onsputter.nif",
-    "meshes/dungeons/dwemer/dwewallsconce02onsputter.nif",
-    "meshes/dungeons/dwemer/dwewallsconcesmall01onsputter.nif",
-    "meshes/dungeons/dwemer/dwewallsconcesmall02onsputter.nif",
-    "meshes/effects/fxdwegreenflamesputter.nif",
-
-    "meshes/dungeons/dwemer/dwefloorlanternon.nif",
-    "meshes/dungeons/dwemer/dwewallsconce01on.nif",
-    "meshes/dungeons/dwemer/dwewallsconce02on.nif",
-    "meshes/dungeons/dwemer/dwewallsconcesmall01on.nif",
-    "meshes/dungeons/dwemer/dwewallsconcesmall02on.nif",
-    "meshes/effects/fxdwegreenflame.nif",
-    "meshes/effects/fxdwegreenflamecalm.nif",
-
-    "meshes/effects/c06fxfirebluewithembersheavy.nif",
-
-    "meshes/effects/fxfirewithembers01_cheap.nif",
-    "meshes/effects/fxfirewithembers03.nif",
-
-    "meshes/effects/fxfiresovngarde.nif",
-
-    "meshes/effects/fxfirewithembers01.nif",
-    "meshes/effects/fxfirewithembers01_lite.nif",
-    "meshes/effects/fxfirewithembers02.nif",
-    "meshes/effects/fxfirewithembers02toggle.nif",
-    "meshes/effects/fxfirewithembersheavytoggle.nif",
-    "meshes/effects/fxfirewithemberslogs01.nif",
-    "meshes/clutter/woodfires/campfire01burning.nif",
-    "meshes/clutter/giant/giantcampfire01burning.nif",
-    "meshes/clutter/woodfires/fireplacewood01burning.nif",
-    "meshes/clutter/woodfires/campfire01landburning.nif",
-
-    "meshes/clutter/giant/giantcampfire01burning.nif",
-    "meshes/clutter/giant/giantcampfire01burningnoland.nif",
-
-    "meshes/dlc02/effects/dlc2castlekarstaagtorch.nif",
-    "meshes/effects/tg09nocturnalfxfirepurple.nif",
-    "meshes/dlc01/dungeons/castle/animated/cascoffinpuzzlebrazier/cascoffinpuzzlebrazier01.nif",
-
-    "meshes/clutter/common/removabletorch01.nif",
-    "meshes/clutter/common/torchpermanent01.nif",
-    "meshes/clutter/common/torchsconce01.nif",
-
-    "meshes/dlc01/soulcairn/sc_crater01.nif",
-    "meshes/dlc01/soulcairn/sc_lightwell01.nif",
-
-    "meshes/dlc02/dungeons/apocrypha/animated/aposcryetrigger01/aposcryetrigger01.nif",
-
-    "meshes/dlc02/dungeons/apocrypha/clutter/apolightpost01.nif",
-    "meshes/dlc02/dungeons/apocrypha/clutter/apolightpost02.nif",
-
-    "meshes/dlc02/dungeons/apocrypha/clutter/apoheadscone02.nif",
-    "meshes/dlc02/dungeons/apocrypha/clutter/apomouthsconce02.nif",
-
-    "meshes/dlc02/dungeons/apocrypha/clutter/apoheadscone03.nif",
-    "meshes/dlc02/dungeons/apocrypha/clutter/apomouthsconce03.nif"
-};
-
+std::unordered_set<std::string> allMeshes{
+    "candlehornfloor01",
+    "candlehorntable01",
+    "candlehornwall01",
+    "glazedcandlesstatic02",
+    "glazedcandles01",
+    "mpscandleflame01.nif",
+    "candlelanternwithcandle01",
+    "impcandelabracandle01",
+    "impcandelabracandle02",
+    "impcandle01",
+    "impwallsconce02candleon01",
+    "impwallsconcecandle01",
+    "impwallsconcecandle02",
+    "candlehornchandelier01",
+    "candlehornchandelier02",
+    "impchandelliercandle01",
+    "impchandelliercandle02",
+    "mgmagicfirepillarsmall",
+    "mgmagicfirepillarmidden01",
+    "mgmagicfirepillar01",
+    "darkelflantern",
+    "dwechandelier01bareonsputter",
+    "dwechandelier01onsputter",
+    "dwechandelier01simpleonsputter",
+    "dwechandelier02onsputter",
+    "dwechandelier01bareon",
+    "dwechandelier01on",
+    "dwechandelier01simpleon",
+    "dwechandelier02on",
+    "dwefloorlanternonsputter",
+    "dwewallsconce01onsputter",
+    "dwewallsconce02onsputter",
+    "dwewallsconcesmall01onsputter",
+    "dwewallsconcesmall02onsputter",
+    "fxdwegreenflamesputter",
+    "dwefloorlanternon",
+    "dwewallsconce01on",
+    "dwewallsconce02on",
+    "dwewallsconcesmall01on",
+    "dwewallsconcesmall02on",
+    "fxdwegreenflame",
+    "fxdwegreenflamecalm",
+    "c06fxfirebluewithembersheavy",
+    "fxfirewithembers01_cheap",
+    "fxfirewithembers03",
+    "fxfiresovngarde",
+    "fxfirewithembers01",
+    "fxfirewithembers01_lite",
+    "fxfirewithembers02",
+    "fxfirewithembers02toggle",
+    "fxfirewithembersheavytoggle",
+    "fxfirewithemberslogs01",
+    "campfire01burning",
+    "giantcampfire01burning",
+    "fireplacewood01burning",
+    "campfire01landburning",
+    "giantcampfire01burningnoland",
+    "dlc2castlekarstaagtorch",
+    "tg09nocturnalfxfirepurple",
+    "cascoffinpuzzlebrazier01",
+    "removabletorch01",
+    "torchpermanent01",
+    "torchsconce01",
+    "sc_crater01",
+    "sc_lightwell01",
+    "aposcryetrigger01",
+    "apolightpost01",
+    "apolightpost02",
+    "apoheadscone02",
+    "apomouthsconce02",
+    "apoheadscone03",
+    "apomouthsconce03"
+};*/
     
 std::string forswornFires = "";
 std::string vampireFires = "";
@@ -292,3 +226,5 @@ RE::BGSKeyword* keywordForswornCamp = nullptr;
 RE::BGSKeyword* keywordVampireLair = nullptr;
 RE::BGSKeyword* keywordDungeon = nullptr;
 RE::BGSKeyword* keywordDragonPriestLair = nullptr;
+
+int dataHasLoaded = 0; 
