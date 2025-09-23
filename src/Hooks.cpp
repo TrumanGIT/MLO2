3b8fb0e635ec02c84f68feac9d73613a9e066e03
Easychips <saberxnekopokemoncheezer@gmail.com>
2025-09-22 21:34:42 -0500

    new clone banking system
    before we cloned ninodes from disk and attached them during gameplay. to save some juice we now store most commonly used light nodes ie candle , fires, lanterns in a bank we fill on game load. we then use them when needed cutting back on cloneing nodes during gamplay.

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -7,7 +7,9 @@
 #include <unordered_set>
 
 namespace Hooks {
-    // Thiago99's
+
+    // Thiago99s
+
     /*bool Hooks::ReplaceTextureOnObjectsHook::ShouldBackgroundClone(RE::TESObjectREFR* ref) {
        if (ref){
            ProcessReference(ref);
@@ -23,20 +25,23 @@ namespace Hooks {
     */
 
     //Po3's
+
     RE::NiPointLight* TESObjectLIGH_GenDynamic::thunk(RE::TESObjectLIGH* light, RE::TESObjectREFR* ref,
-                                                      RE::NiNode* node, bool forceDynamic, bool useLightRadius,
-                                                      bool affectRequesterOnly) {
+        RE::NiNode* node, bool forceDynamic, bool useLightRadius,
+        bool affectRequesterOnly) {
 
         if (should_disable_light(light, ref)) return nullptr;
 
-        light->data.color.red = 255;
+        auto refOriginFile = ref->GetDescriptionOwnerFile();
 
-        light->data.color.green = 161;
-
-        light->data.color.blue = 60;
-
+        if (std::string(refOriginFile->fileName) != "Window Shadows Ultimate.esp") {
+            light->data.color.red = 255;
+            light->data.color.green = 161;
+            light->data.color.blue = 60;
+        }
         return func(light, ref, node, forceDynamic, useLightRadius, affectRequesterOnly);
     }
+  
 
     void TESObjectLIGH_GenDynamic::Install() {
         std::array targets{
@@ -51,42 +56,43 @@ namespace Hooks {
 
         logger::info("Installed TESObjectLIGH::GenDynamic patch");
     }
+
     //Po3's
-void PostCreate::thunk(
-    RE::TESModelDB::TESProcessor* a_this,
-    const RE::BSModelDB::DBTraits::ArgsType& a_args,
-    const char* a_nifPath,
-    RE::NiPointer<RE::NiNode>& a_root,
-    std::uint32_t& a_typeOut)
-{
-    if (dataHasLoaded != 1 || !a_root) {
-        return func(a_this, a_args, a_nifPath, a_root, a_typeOut);
-    }
 
-    thread_local bool inHook = false;
-    if (inHook) {
-        return func(a_this, a_args, a_nifPath, a_root, a_typeOut);
-    }
-    struct Guard { bool& f; Guard(bool& v):f(v){f=true;} ~Guard(){f=false;} } guard(inHook);
-
-    std::string nodeName = a_root->name.c_str();
-    toLower(nodeName);
-
-    auto it = baseMeshesAndNiNodeToAttach.find(nodeName);
-    if (it != baseMeshesAndNiNodeToAttach.end()) {
-        logger::info("Attaching animated node to {}", nodeName);
-
-        std::string fullPath = "Meshes\\NEW\\SSE\\" + it->second;
-        auto nodeClone = cloneNiNode(fullPath);
-        if (!nodeClone) {
-            logger::warn("Failed to clone template for {}", nodeName);
-        } else {
-            AttachNodeToMesh(a_root.get(), nodeClone.get());
+    void PostCreate::thunk(
+        RE::TESModelDB::TESProcessor* a_this,
+        const RE::BSModelDB::DBTraits::ArgsType& a_args,
+        const char* a_nifPath,
+        RE::NiPointer<RE::NiNode>& a_root,
+        std::uint32_t& a_typeOut)
+    {
+        if (!dataHasLoaded || !a_root) {
+            return func(a_this, a_args, a_nifPath, a_root, a_typeOut);
+            logger::info("no root or data hasent loaded cancelling hook(intentional)");
         }
-    }
 
-    func(a_this, a_args, a_nifPath, a_root, a_typeOut);
-}
+        std::string nodeName = a_root->name.c_str(); // grab name of ni node usually 1:1 with mesh names thank god. 
+        toLower(nodeName);                           //some nodes are called dummy you can see in dummyhandler() how i deal with that. (thx bethesda)
+    //    logger::info("incoming node = {}", nodeName);
+        if (!cloneAndAttachNodesForSpecificMeshes(nodeName, a_root)) { // look for specific meshes first
+
+            auto match = matchedKeyword(nodeName); // then check for keywords to cover a large net ie candle, lantern ect
+
+            if (!match.empty()) {
+
+                dummyHandler(a_root.get(), nodeName); // then deal with dummy nodes. I should mention,
+                                                      // file paths with this hook are always null except for loose files. thats why we check by node name. 
+                                                      //     
+                if (nodeName != "lantern") { // nodes with just lantern are a empty lantern so we need to exclude them getting light
+
+                    RE::NiPointer<RE::NiNode> nodePtr = getNextNodeFromBank(match);
+                    AttachNodeToMesh(a_root.get(), nodePtr.get());
+                }
+            }
+        }
+
+        func(a_this, a_args, a_nifPath, a_root, a_typeOut);
+    }
 
 
 void PostCreate::Install() {
@@ -98,8 +104,6 @@ void PostCreate::Install() {
 
     logger::info("Installed TESModelDB::TESProcessor hook");
 }
-
-
     void Install() {
     SKSE::AllocTrampoline(1 << 8);
     TESObjectLIGH_GenDynamic::Install();
