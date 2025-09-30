cd02ba97e7a3319e079c0ef2410c2a6f0b0eb637
Easychips <saberxnekopokemoncheezer@gmail.com>
2025-09-30 17:31:12 -0500

    Added exclusion list

--- src/global.cpp
+++ src/global.cpp
@@ -12,6 +12,7 @@ uint8_t red = 255;
 uint8_t green = 161;
 uint8_t blue = 60;
 
+
 bool dataHasLoaded = false;
 
 std::array<RE::FormID, 4> fireFormIDs = {0x00033DA4, 0x00033DA9, 0x000CD823, 0x000CD824};
@@ -24,16 +25,20 @@ std::array<RE::FormID, 3> dungeonOutlierFiresIDs = {
 
 std::vector<std::string> whitelist;
 
+std::vector<std::string> exclusionList; 
+
+std::vector<std::string> exclusionListPartialMatch;
+
 std::unordered_map<RE::TESModel*, std::string> ModelsAndOriginalFilePaths;
 
 // nodeName (lowercased) -> template mesh path
 std::unordered_map<std::string, std::string> baseMeshesAndTemplateToAttach = {};
 
-
 std::unordered_map<std::string, std::string> keywordTemplateMap = {};
 
 std::map<std::string, std::array<RE::NiPointer<RE::NiNode>, 75>> keywordNodeBank = {
-    {"chandeliers", {}}, // "Chandeliers_NOT_Animated.nif"
+    {"dwec", {}},        // "Dwemer Chandelier Sputter_Animated.nif"
+    {"chandelier", {}}, // "Chandeliers_NOT_Animated.nif"
     {"candle", {}},      // "Candles_NOT_Animated.nif"
     {"fxfirew", {}},     // "Fires_Animated.nif"
     {"campfire", {}},    // "Fires_Animated.nif"
@@ -42,9 +47,7 @@ std::map<std::string, std::array<RE::NiPointer<RE::NiNode>, 75>> keywordNodeBank
         {"torch", {}}, // Hand Held Torch_Animated.nif
         {"torchsconce", {}}, // "Torch Sconce_Animated.nif"    
     {"lantern", {}},     // "Candles_NOT_Animated.nif"
-    {"dwec", {}},        // "Dwemer Chandelier Sputter_Animated.nif"
     {"dwewalls", {}},   // "Dwemer Light Sputter_Animated.nif"
-    { "dummy", {}},
 };
 
 std::string forswornFires = "";
