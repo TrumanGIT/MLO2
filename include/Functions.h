48bf1dc399e0dd1c590bd6a2fe50fab5dadefbab
Easychips <saberxnekopokemoncheezer@gmail.com>
2025-10-06 08:21:33 -0500

    Update Functions.h

--- include/Functions.h
+++ include/Functions.h
@@ -6,7 +6,6 @@
 #include <map>
 #include <unordered_set>
 #include <sstream>
-#include <iostream>
 
 namespace logger = SKSE::log;
 
@@ -34,7 +33,7 @@ namespace logger = SKSE::log;
         logger::info("BGSKeyword LocTypeDragonPriestLair (0x000130E1) not found");
     }
 }
-
+ // clode a ni node froma  template ahead of time and assign as a value to a array of meshes
  inline RE::NiPointer<RE::NiNode> cloneNiNode(std::string templatePath) {
 
      RE::NiPointer<RE::NiNode> loaded;
@@ -44,28 +43,26 @@ namespace logger = SKSE::log;
 
      if (result == RE::BSResource::ErrorCode::kNone && loaded) {
 
-      //   logger::info("Loaded root node type: {}", loaded->GetRTTI()->GetName());
-       //  logger::info("Loaded root children count: {}", loaded->children.size());
+         logger::info("Loaded root node type: {}", loaded->GetRTTI()->GetName());
+         logger::info("Loaded root children count: {}", loaded->children.size());
 
-        // if (!loaded->children.empty() && loaded->children[0]) {
-           //  auto firstChild = loaded->children[0]->AsNode();
-             //logger::info("First child node type: {}", firstChild->GetRTTI()->GetName());
-            // logger::info("First child children count: {}", firstChild->children.size());
-        // }
+         if (!loaded->children.empty() && loaded->children[0]) {
+             auto firstChild = loaded->children[0]->AsNode();
+             logger::info("First child node type: {}", firstChild->GetRTTI()->GetName());
+             logger::info("First child children count: {}", firstChild->children.size());
+         }
 
          auto fadeNode = loaded->AsNode();
          if (fadeNode && !fadeNode->children.empty()) {
-               auto firstChild = loaded->children[0]->AsNode();
 
-               if (!firstChild || firstChild->children.empty()) return nullptr;
              RE::NiCloningProcess cloningProcess;
-             auto cloneBase = firstChild->CreateClone(cloningProcess);
-             //firstChild->ProcessClone(cloningProcess);
+             auto cloneBase = fadeNode->CreateClone(cloningProcess);
+             fadeNode->ProcessClone(cloningProcess);
 
              if (cloneBase) {
                  auto yourGlowNodePrototype = cloneBase->AsNode();
                  if (yourGlowNodePrototype) {
-                   //  logger::info("Successfully extracted and cloned prototype from NIF!");
+                     logger::info("Successfully extracted and cloned prototype from NIF!");
                      return RE::NiPointer<RE::NiNode>(yourGlowNodePrototype);
                  }
                  else {
@@ -86,161 +83,28 @@ namespace logger = SKSE::log;
 
      }
      else {
-         logger::warn("Failed to load NIF file {}", templatePath);
+         logger::warn("Failed to load NIF file");
          return nullptr;
      }
  }
 
 
- // sort out what arrays of meshes get what template node. NOTUSED I failed in cloning a clone to use as a template rather then load from disk to save perf. clones came out bugged and shining blue light 
-/*inline void assignClonedNodes() {
-    logger::info("assigning cloned nodes... total groups: {}", baseMeshesAndNiNodeToAttach.size());
-
-    std::string prefix = "Meshes\\NEW\\SSE\\";
-    int templateFilePathIterator = 0;
-
-    for (auto& [meshPaths, nodePtr] : baseMeshesAndNiNodeToAttach) {
-      //  logger::info("cloneing nodes for group #{}", templateFilePathIterator);
-
-        if (templateFilePathIterator >= templateNames.size()) {
-            logger::warn("Ran out of template names for map entries at index {}", templateFilePathIterator);
-            break;
-        }
-
-        auto templatePath = prefix + templateNames[templateFilePathIterator];
-        logger::info("Loading template path [{}]", templatePath);
-
-        auto clonedNode = cloneNiNode(templatePath);
-
-        nodePtr = clonedNode;  // store one copy for the group
-
-        templateFilePathIterator++;
+inline void AttachNodeToMesh(RE::NiNode* root, RE::NiNode* nodeToAttach) {
+    if (!root) {
+        logger::warn("AttachNodeToMesh: root is null");
+        return;
     }
 
-    logger::info("Finished assignClonedNodes");
-}*/
-
-
-// this mod checks for lights to edit by node name. some light nodes are called dummy thanks bethesda
-
-inline std::string matchedKeyword(std::string nodeName) {
-    std::string matchedKeyword;
-
-    for (const auto& [keyword, templates] : keywordTemplateMap) {
-        if (nodeName.find(keyword) != std::string::npos) {
-            matchedKeyword = keyword;
-            break;
-        }
+    if (!nodeToAttach) {
+        logger::warn("AttachNodeToMesh: nodeToAttach is null");
+        return;
     }
 
-    return matchedKeyword;
+    root->InsertChildAt(1,nodeToAttach); // append to the mesh's root
+    logger::info("AttachNodeToMesh: inserted child at 1 successfully successfully");
 }
 
-inline bool cloneAndAttachNodesForSpecificMeshes(const std::string& nodeName, RE::NiPointer<RE::NiNode>& a_root) {
-  
-  
-         auto it = baseMeshesAndTemplateToAttach.find(nodeName);
-    if (it != baseMeshesAndTemplateToAttach.end()) {
-    
-        for (const auto& exclude : exclusionList) {
-            if (nodeName == exclude) {
-             //   logger::info("skipping exclude {}", exclude);
-                return true;
-            }
-        }
-
-        for (const auto& exclude : exclusionListPartialMatch) {
-            if (nodeName.find(exclude)) {
-             //   logger::info("skipping exclude {}", exclude); 
-                return true;
-            }
-        }
-
-        std::string fullPath = "Meshes\\MLO\\Templates\\" + it->second;
-        auto nodeClone = cloneNiNode(fullPath);
-        if (!nodeClone) {
-            logger::warn("Failed to clone from template for {}", nodeName);
-            return false;
-        }
-
-        a_root->AttachChild(nodeClone.get());
-        //logger::warn("attached node to specific mesh {} ", nodeName);
-
-        return true;
-    }
-
-    return false;
-}
-
-inline void assignClonedNodesToBank() {
-    logger::info("Assigning cloned nodes... total groups: {}", keywordNodeBank.size());
-
-    std::string prefix = "Meshes\\MLO\\Templates\\";
-
-    for (auto& [keyword, arrayOfNodes] : keywordNodeBank) {
-        auto templateIt = keywordTemplateMap.find(keyword);
-        if (templateIt != keywordTemplateMap.end()) {
-            std::string templatePath = prefix + templateIt->second;
-            logger::info("Template path for {} is {}", keyword, templatePath);
-
-            if (keyword == "candle") {
-                for (size_t i = 0; i < 75; i++) {  // lots of candles lmao
-                    arrayOfNodes[i] = cloneNiNode(templatePath);
-                }
-            }
-            else {
-                for (size_t i = 0; i < 25; i++) {
-                    arrayOfNodes[i] = cloneNiNode(templatePath);
-                }
-            }
-        }
-    }
-
-    logger::info("Finished assignClonedNodes");
-}
-
-
-//we clone and store ni nodes in a bank on startup to help with performance (for nodes we can anyway)
-
-inline RE::NiPointer<RE::NiNode>& getNextNodeFromBank(const std::string& keyword) {
-    auto& bank = keywordNodeBank[keyword];
-
-    static std::size_t chandelierCount = 0;
-    static std::size_t candleCount = 0;
-    static std::size_t fxfirewCount = 0;
-    static std::size_t campfireCount = 0;
-    static std::size_t fireplacewoodCount = 0;
-    static std::size_t torchSconceCount = 0;
-    static std::size_t torchCount = 0;
-    static std::size_t lanternCount = 0;
-    static std::size_t dwecCount = 0;
-    static std::size_t dwewallsCount = 0;
-    static std::size_t firepitCount = 0;
-    static std::size_t errorCount = 0;
-    std::size_t& count = [&]() -> std::size_t& {
-        if (keyword == "chandelier") return chandelierCount;
-        if (keyword == "candle") return candleCount;
-        if (keyword == "fxfirew") return fxfirewCount;
-        if (keyword == "campfire") return campfireCount;
-        if (keyword == "fireplacewood") return fireplacewoodCount;
-        if (keyword == "torchsconce") return torchSconceCount;
-        if (keyword == "torch") return torchCount;
-        if (keyword == "lantern") return lanternCount;
-        if (keyword == "dwec") return dwecCount;
-        if (keyword == "dwewalls") return dwewallsCount;
-        if (keyword == "firepit") return firepitCount;
-        return errorCount; // fallback
-        }();
-
-    RE::NiPointer<RE::NiNode>& node = bank[count];
-    count = (count + 1) % bank.size(); // this could cause infinite loop ? 
-
-  // logger::info("Keyword '{}' used node index = {}", keyword, count);
-
-    return node;
-}
-
-// stole this from somewhere Po3 orz
+// this is only if we need to overwrite 6 bits of memory instead of the default 5.... currently not used. ive tried alot of hooks lmao
 
 template <class T, std::size_t size = 5>  
 inline void write_thunk_call(std::uintptr_t a_src) {
@@ -252,7 +116,7 @@ inline void write_thunk_call(std::uintptr_t a_src) {
     }
 }
 
-// checkcs if fake lights should be disabled by checking some user settings. and excluding dynamicform lights
+// makes sure were not disabling dynamically spawned lights for quests like the moondial light in volkihar courtyard in DLC1 
 // or whitelisted lights by checking the plugin name or carryable or shadowcasters lol
 
 inline bool should_disable_light(RE::TESObjectLIGH* light, RE::TESObjectREFR* ref) 
@@ -261,7 +125,7 @@ inline bool should_disable_light(RE::TESObjectLIGH* light, RE::TESObjectREFR* re
         return false;
     }
 
-    if (disableShadowCasters != 1 &&
+    if (!disableShadowCasters &&
         light->data.flags.any(RE::TES_LIGHT_FLAGS::kOmniShadow,
                               RE::TES_LIGHT_FLAGS::kHemiShadow,
                               RE::TES_LIGHT_FLAGS::kSpotShadow)) 
@@ -269,17 +133,17 @@ inline bool should_disable_light(RE::TESObjectLIGH* light, RE::TESObjectREFR* re
         return false;
     }
 
-    if (disableTorchLights != 1 &&
+    if (!disableTorchLights &&
         light->data.flags.any(RE::TES_LIGHT_FLAGS::kCanCarry)) 
     {
         // disable lights that can be carried (i.e., torches)
         return false;
     }
-    
+
     auto refOriginFile = ref->GetDescriptionOwnerFile();
     for (auto fileName : whitelist) {
         if (fileName == refOriginFile->fileName) {
-           // logger::info("found whitelisted light");
+            logger::info("found whitelisted light");
             return false;
         }
     }
@@ -287,7 +151,7 @@ inline bool should_disable_light(RE::TESObjectLIGH* light, RE::TESObjectREFR* re
     return true;
 }
 
-// method to swap fire color models not used anymore see below
+// method to swap fire color models
 
 inline void ApplyColorSwitch(RE::TESModel* bm, const std::string& newPath) {
     if (!bm) return;
@@ -300,7 +164,6 @@ inline void ApplyColorSwitch(RE::TESModel* bm, const std::string& newPath) {
     }
 }
 
-/// this isent used anymore. it was to change colors of fires in certain locations but the feature got deprecated from the mod
 inline void ProcessReference(RE::TESObjectREFR* a_ref) {
    // const auto refid = a_ref->GetFormID();
     const auto base = a_ref->GetBaseObject();
@@ -353,7 +216,6 @@ inline void ProcessReference(RE::TESObjectREFR* a_ref) {
         }
     }
 
-// bad naming this also adds items to whitelist 
 inline void splitString(const std::string& input, char delimter, std::vector<std::string>& listToSplit) {
     std::stringstream ss(input);
     std::string item;
@@ -369,12 +231,12 @@ inline void splitString(const std::string& input, char delimter, std::vector<std
 
         listToSplit.push_back(item);
 
-        spdlog::info("added {} to {}", item, listToSplit);
+        spdlog::info("added {} to whitelist", item);
     }
 }
 
 inline void IniParser() {
-    std::ifstream iniFile("Data\\SKSE\\Plugins\\MLO.ini");
+    std::ifstream iniFile("Data\\SKSE\\Plugins\\MLO2.ini");
     std::string line;
 
     while (std::getline(iniFile, line)) {
@@ -382,7 +244,7 @@ inline void IniParser() {
         if (line.empty() || line[0] == ';') continue;
 
         if (line.starts_with("disableShadowCasters=")) {
-            disableShadowCasters = std::stoi(line.substr(std::string("disableShadowCasters=").length()));
+            disableShadowCasters = std::stof(line.substr(std::string("disableShadowCasters=").length()));
             spdlog::info("INI override: disableShadowCasters = {}", disableShadowCasters);
 
         } else if (line.starts_with("disableTorchLights=")) {
@@ -404,27 +266,12 @@ inline void IniParser() {
         } else if (line.starts_with("dragonPriestFires=")) {
             dragonPriestFires = line.substr(std::string("dragonPriestFires=").length());
             spdlog::info("INI override: dragonPriestFires = {}", dragonPriestFires);
-        }
-        else if (line.starts_with("RGB Values=")) {
-            auto values = line.substr(std::string("RGB Values=").length());
-
-            // Find each color
-            auto rPos = values.find("Red:");
-            auto gPos = values.find("Green:");
-            auto bPos = values.find("Blue:");
-
-            if (rPos != std::string::npos) {
-                red = static_cast<std::uint8_t>(std::stoi(values.substr(rPos + 4)));
-            }
-            if (gPos != std::string::npos) {
-                green = static_cast<std::uint8_t>(std::stoi(values.substr(gPos + 6)));
-            }
-            if (bPos != std::string::npos) {
-                blue = static_cast<std::uint8_t>(std::stoi(values.substr(bPos + 5)));
-            }
-
-            spdlog::info("INI override: Bulb RGB values set to R:{} G:{} B:{}", red, green, blue);
-        }
+        } else if (line.starts_with("RGB Values=")){
+            Red = std::stoi(line.substr(std::string("red:").length()));
+            blue = std::stoi(line.substr(std::string("blue:").length()));
+            green = std::stoi(line.substr(std::string("green:").length()));
+             spdlog::info("INI override: Bulb RBG values set to R:{} G:{} B:{}", red, green, blue);
+        } 
 
         else if (line.starts_with("whitelist=")) {
             std::string prefix = "whitelist=";
@@ -433,19 +280,19 @@ inline void IniParser() {
 
             splitString(line, ',', whitelist);
         }
-
-
     }
+
+    iniFile.close(); 
 }
 
-inline std::string trim(const std::string& s) {
+std::string trim(const std::string& s) {
     size_t start = s.find_first_not_of(" \t");
     size_t end = s.find_last_not_of(" \t");
     return (start == std::string::npos) ? "" : s.substr(start, end - start + 1);
 }
 
-inline void ReadMasterListAndFillMap() {
-    std::string path = "Data\\SKSE\\Plugins\\Master.Ini";
+inline void BuildMasterList() {
+    std::string path = "Data\\SKSE\\Plugins\\MLO2.ini";
 
     if (!std::filesystem::exists(path)) {
         std::cerr << "INI file not found: " << path << std::endl;
@@ -470,120 +317,27 @@ inline void ReadMasterListAndFillMap() {
         if (line.starts_with(";")) {
             commentCount++;
             if (commentCount >= 2) mapPtr = &keywordTemplateMap; // stop reading after second comment block
-            if (commentCount >= 3) break; 
-            continue;
+            continue; 
         }
 
-        auto pos = line.find('=');
+        auto pos = line.find(':');
         if (pos != std::string::npos) {
             std::string key = trim(line.substr(0, pos));
             std::string value = trim(line.substr(pos + 1));
             (*mapPtr)[key] = value; // dereference,  [] > * in operator precedence. (*mapPtr) is evaluated first, giving you the actual map.
         }
-
     }
 
     iniFile.close();
 }
 
 
-inline void toLower(std::string& str) {
+inline void toLower(std::string& str) { 
     for (auto& c : str) {
         c = static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
     }
 }
 
-
-inline void ReadMasterListAndFillExcludes() {
-    std::string path = "Data\\SKSE\\Plugins\\Master.Ini";
-
-    if (!std::filesystem::exists(path)) {
-        logger::warn("INI file not found: {}", path);
-        return;
-    }
-
-    std::ifstream iniFile(path);
-    if (!iniFile.is_open()) {
-        logger::warn("Failed to open INI file: {}", path);
-        return;
-    }
-
-    std::string line;
-    int section = 0; // 0=normal, 1=exact excludes, 2=partial excludes
-
-    while (std::getline(iniFile, line)) {
-        line = trim(line);
-        if (line.empty())
-            continue;
-
-        if (line.starts_with(";")) {
-            // detect section headers
-            if (line.find("Exclude by node name exact match") != std::string::npos) {
-                section = 1;
-            }
-            else if (line.find("Exclude by node name partial match") != std::string::npos) {
-                section = 2;
-            }
-            continue;
-        }
-
-        if (section == 1) {
-            line = trim(line);  // already trimming, good
-            toLower(line);      // lowercase for consistency
-            exclusionList.push_back(line);
-            logger::info("Added exact exclude: '{}'", line);  // wrap in quotes to see trailing whitespace
-        }
-        else if (section == 2) {
-            line = trim(line);
-            toLower(line);
-            exclusionListPartialMatch.push_back(line);
-            logger::info("Added partial exclude: '{}'", line);
-        }
-    }
-
-    iniFile.close();
-}
-
-   
-
-
-inline void dummyHandler(RE::NiNode* root, std::string nodeName)
-{
-    if (!root) return;
-
-    // Only operate on nodes whose own name contains "dummy"
-  
-    if (nodeName.find("dummy") == std::string::npos)
-        return;
-
-    // Search children for a NiNode whose name contains "candle"
-    for (auto& child : root->children) {
-        if (!child) continue;
-
-        auto niNodeChild = child->AsNode();
-        if (!niNodeChild) continue;
-
-        std::string childName = niNodeChild->name.c_str();
-        toLower(childName);
-
-        if (childName.find("chandelier") != std::string::npos) {
-            RE::NiPointer<RE::NiNode> nodePtr = getNextNodeFromBank("chandelier");
-            root->AttachChild(nodePtr.get());
-            // logger::info(" attached light ot dummy node {}", rootName);
-            return; // attach only once
-        }
-
-        if (childName.find("candle") != std::string::npos) {
-            RE::NiPointer<RE::NiNode> nodePtr = getNextNodeFromBank("candle");
-            root->AttachChild(nodePtr.get());
-           // logger::info(" attached light ot dummy node {}", rootName);
-            return; // attach only once
-        }
-
-    }
-
-    // No child contained "candle" — do nothing
-}
 
 inline void DumpFullTree(RE::NiAVObject* obj, int depth = 0)
 {
