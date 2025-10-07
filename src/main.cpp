929cfcac700039ff87dc53a302d2ea310e93f4f8
Easychips <saberxnekopokemoncheezer@gmail.com>
2025-10-06 22:43:36 -0500

    Cleaned up

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_s
 
     SKSE::Init(a_skse);
     IniParser();
-    ReadMasterListAndFillMap();
+    ReadMasterListAndFillMaps();
     ReadMasterListAndFillExcludes();
     SKSE::AllocTrampoline(1 << 10);
     g_messaging->RegisterListener("SKSE", SKSEMessageHandler);
